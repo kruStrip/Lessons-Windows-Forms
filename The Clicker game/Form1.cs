e6bd20c6081045273ad2ebef02a126c2d0406742
Катя <katya.kotel@bk.ru>
2025-06-05 11:10:15 +0300

    wxs

---
+++ The Clicker game/Form1.cs
@@ -0,0 +1,15 @@
+namespace The_Clicker_game
+{
+    public partial class Тапалка : Form
+    {
+        public Тапалка()
+        {
+            InitializeComponent();
+        }
+
+        private void progressBar1_Click(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
